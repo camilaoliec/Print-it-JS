50e7f5b50fb7bd4dbe77bbe44004855f4a3888cb
Camila CROSET <potycamila@gmail.com>
2024-08-20 04:02:56 +0200

    fix: correction de l'organisation des dossiers du projet

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,4 @@
+document.addEventListener('DOMContentLoaded',() => {
 const slides = [
 	{
 		"image":"slide1.jpg",
@@ -15,4 +16,36 @@ const slides = [
 		"image":"slide4.png",
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
-]
+];
+
+// Event listener - fleches
+
+const arrowLeft = document.querySelector('.arrow_left');
+const arrowRight = document.querySelector('.arrow_right');
+
+let currentSlide = 0;
+let image = document.querySelector(".banner-img");
+let bannerText = document.querySelector('banner p');
+
+function updateCarousel(){
+	image.src = "./assets/images/slideshow/" + slides[currentSlide]["image"];
+	bannerText.innerHTML = slides[currentSlide]["image"];
+}
+
+arrowLeft.addEventListener('click', () => {
+	console.log('clique sur la gauche');
+	alert('fleche gauche cliqué')
+	currentSlide = (currentSlide === 0) ? slides.length -1: currentSlide -1;
+	updateCarousel();
+});
+arrowRight.addEventListener('click', () => {
+	console.log('Clique sur la droite');
+	alert('Fleche droite cliqué')
+	currentSlide = (currentSlide === slides.length -1) ? 0 : currentSlide + 1; 
+	updateCarousel();
+	console.log('click sur la flece droite');
+});
+
+updateCarousel()
+
+});
